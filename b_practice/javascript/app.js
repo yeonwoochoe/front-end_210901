3cffa6a665508391beff127f0481182f8172dc87
yeonwoochoe <chiyy6162@gmail.com>
2021-10-15 09:36:06 +0900

    js 연습
    js 연습

--- b_practice/javascript/app.js
+++ b_practice/javascript/app.js
@@ -1,3 +1,11 @@
-const amIFat = null;
-let someting;
-console.log(someting);
+const mon = "mon";
+const tue = "tue";
+const web = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const days0wfweek = mon + tue + web + thu + fri + sat + sun;
+
+console.log(days0wfweek);
