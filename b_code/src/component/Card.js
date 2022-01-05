93a3a4748c7f453c6b7bff401d4a736edabd6972
YEONWOO <89890240+yeonwoochoe@users.noreply.github.com>
2022-01-05 17:34:46 +0900

    [feat] react 🆕 useState, useEfftect 상태 복습

--- b_code/src/component/Card.js
+++ b_code/src/component/Card.js
@@ -1,42 +1,51 @@
-//Card.js
+// Card.js
 import { useState } from 'react';
 import { MdFavorite } from 'react-icons/md';
-export default function Card({data}){
 
+export default function Card({data}){  
+  
   const btnStyleArr = [
-    {color:'#555', fontWeight:700, backgroundColor:'#fff', border: '1px solid #777'},
-    {color:'#555', fontWeight:700, backgroundColor:'#afa'},
-    ]
+    {color:'#333', fontWeight:700, backgroundColor:'#ada'},
+    {color:'#155', fontWeight:700, backgroundColor:'#fff', border:'1px solid #777'}
+  ];
+  const styleArr = [ {color:'#333'}, {color:'#f00'} ];
 
-  const styleArr = [
-    {color: '#333'},
-    {color: '#f33'}
-  ]
+
   const [ckN, setCkN] = useState(0);
-  const fnCheck = ()=>{ setCkN( ()=> ckN === 0 ? 1 : 0 )};
+  const [checkState, setCheckState] = useState('');
+  const fnCheck = (e)=>{
+    setCkN( ()=> ckN === 0 ? 1 : 0 )
+    setCheckState( e.target.checked );
+  };
+
   return (
     <li>
-    <div  className="img_set" 
-          style={ {backgroundColor:data.imgUrl} }> {data.imgAlt}
-    </div>
-    <dl>
-      <dt>{data.title}</dt>
-      <dd>{data.content}</dd>
-    </dl>
-    <div className="btns">
-      <form className="blind">
+      <div  className="img_set" 
+            style={ {backgroundColor:data.imgUrl} }> 
+            {data.imgAlt}
+      </div>
+      <dl>
+        <dt>{data.title}</dt>
+        <dd>{data.content}</dd>
+      </dl>
+      <div className="btns">
+  
+        <input  className="blind"
+                type="checkbox"
+                id={data.checkName} 
+                name={data.checkName} 
+                onChange={ fnCheck }
+                data-checked={checkState}
+                value="좋아요체크" />
+        
+        <label  htmlFor={data.checkName} 
+                type="button" 
+                style={btnStyleArr[ckN]}>
+          좋아요 <MdFavorite style={styleArr[ckN]} />
+        </label>
 
-      <input type="checkbox" 
-            id={data.id} 
-            name={data.id} 
-            value="좋아요 체크" /> 
-
-      <label type="button" 
-              onClick={fnCheck} style={btnStyleArr[ckN]} >  좋아요 <MdFavorite style={styleArr[ckN]}/></label>
-
-      </form>
-      <a className="link" href={data.detailLInk}>상세보기</a>
-    </div>
-  </li>
+        <a className="link" href={data.detailLInk}>상세보기</a>
+      </div>
+    </li>
   )
 }
