8abcdebad5bf67205c1d9507f6a3d266e140845f
YEONWOO <89890240+yeonwoochoe@users.noreply.github.com>
2022-01-10 14:55:09 +0900

    [feat] react 🆕 react router dom 복습 및  개념정리

--- b_code/src/component/Footer.js
+++ b_code/src/component/Footer.js
@@ -1,8 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../style/Footer.scss';
 
 export default function Footer(props){
+
+  const fnAction = ({isActive})=> isActive ? "on" : '' ;
+
   return (
-    <footer>
+    <footer id="footBox">
       <h2>{props.heading} 정보</h2>
+      <div className="company_info">
+        <ul>
+          <li><NavLink className={fnAction} to="/manual">이용약관</NavLink></li>
+          <li><NavLink className={fnAction} to="/person">개인정보처리</NavLink></li>
+          <li><NavLink className={fnAction} to="/rule">회사방침</NavLink></li>
+          <li><NavLink className={fnAction} to="/etc">기타</NavLink></li>
+        </ul>
+      </div>
     </footer>
   )
 }
